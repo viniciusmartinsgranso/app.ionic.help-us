7b0dbfd87463ee296fbcb8b1a723adf8625fa11f
unknown <manut@C-264.labinfo.facens.br>
2024-04-20 03:35:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'help-us-front',
+  webDir: 'www'
+};
+
+export default config;
